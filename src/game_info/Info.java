631caa10095c94948ef32eafb99003b77598541a
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-03-15 16:45:57 -0500

    Added default colors (Background, foreground, menu particle) to the Info interface

--- src/game_info/Info.java
+++ src/game_info/Info.java
@@ -7,4 +7,6 @@ public interface Info {
     int CANVAS_WIDTH = 800;
     int CANVAS_HEIGHT = 600;
     Color BACKGROUND_COLOR = Color.BLACK;
+    Color FOREGROUND_COlOR = Color.WHITE;
+    Color MENU_PARTICLE_COLOR = Color.GRAY;
 }
