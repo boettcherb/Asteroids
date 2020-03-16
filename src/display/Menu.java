7bb0501e0f5aead2a97e74bf76fd1d1d575f4ce8
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-03-16 17:09:46 -0500

    Added buttons and the title to the main menu screen.

--- src/display/Menu.java
+++ src/display/Menu.java
@@ -1,18 +1,23 @@
 package display;
 
 import game_info.Info;
+import util.Button;
 
 import java.awt.Graphics;
 import java.util.Random;
 
 public class Menu implements Info {
     private final Particle[] particles;
+    private Button play, help, quit;
 
     public Menu() {
         particles = new Particle[NUM_PARTICLES];
         for (int i = 0; i < particles.length; ++i) {
             particles[i] = new Particle();
         }
+        play = new Button(PLAY_BUTTON_TEXT, PLAY_BUTTON_HEIGHT);
+        help = new Button(HELP_BUTTON_TEXT, HELP_BUTTON_HEIGHT);
+        quit = new Button(QUIT_BUTTON_TEXT, QUIT_BUTTON_HEIGHT);
     }
 
     public void tick() {
@@ -26,6 +31,11 @@ public class Menu implements Info {
         for (Particle p : particles) {
             p.draw(g);
         }
+        g.setColor(FOREGROUND_COlOR);
+        Button.drawCenteredString(g, GAME_TITLE, TITLE_RECT, TITLE_FONT);
+        play.draw(g);
+        help.draw(g);
+        quit.draw(g);
     }
 
     private static class Particle {
@@ -37,8 +47,8 @@ public class Menu implements Info {
         }
 
         private void reset() {
-            x = rand.nextInt(CANVAS_WIDTH) - CANVAS_WIDTH / 2.0; // value between -WIDTH/2 and WIDTH/2
-            y = rand.nextInt(CANVAS_HEIGHT) - CANVAS_HEIGHT / 2.0; // value between -HEIGHT/2 and HEIGHT/2
+            x = rand.nextInt(CANVAS_WIDTH) - CANVAS_WIDTH / 2.0;
+            y = rand.nextInt(CANVAS_HEIGHT) - CANVAS_HEIGHT / 2.0;
             depth = CANVAS_WIDTH / 2.0;
         }
 
