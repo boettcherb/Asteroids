78db5e29c23fb5ca368c10c4ba8e634a7ba83276
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-03-16 17:04:56 -0500

    Updated the Button class. Each button has a string and the class will draw that string in the center of the button.

--- src/util/Button.java
+++ src/util/Button.java
@@ -2,22 +2,32 @@ package util;
 
 import game_info.Info;
 
-import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.*;
 
 public class Button implements Info {
+    private final String buttonText;
     private final int X, Y;
 
-    public Button(double percentHeight) {
+    public Button(String text, float percentHeight) {
+        buttonText = text;
         Y = (int) (percentHeight * CANVAS_HEIGHT);
         X = (CANVAS_WIDTH / 2) - (BUTTON_WIDTH / 2);
     }
 
     public void draw(Graphics g) {
         g.drawRect(X, Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+        drawCenteredString(g, buttonText, getRectangle(), BUTTON_FONT);
     }
 
     public Rectangle getRectangle() {
         return new Rectangle(X, Y, BUTTON_WIDTH, BUTTON_HEIGHT);
+    }
+
+    public static void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) {
+        FontMetrics metrics = g.getFontMetrics(font);
+        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        g.setFont(font);
+        g.drawString(text, x, y);
     }
 }
