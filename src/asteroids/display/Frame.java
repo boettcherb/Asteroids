60f70b8683867a65ea4c30939bfe3ff71c50a617
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-03-30 21:24:47 -0500

    Added javadocs and comments to the Frame class.

--- src/asteroids/display/Frame.java
+++ src/asteroids/display/Frame.java
@@ -17,24 +17,18 @@ public class Frame implements Info {
      */
     public Frame(GUI gui) {
         frame = new JFrame(FRAME_TITLE);
-
         // make sure the size of the frame takes into account any title bars or side bars
         // (otherwise a portion of the canvas would be invisible)
         int frameWidth = CANVAS_WIDTH + frame.getInsets().left + frame.getInsets().right;
         int frameHeight = CANVAS_HEIGHT + frame.getInsets().top + frame.getInsets().bottom;
-
         // set the size of the frame
         setDimensions(new Dimension(frameWidth, frameHeight));
-
         // make the red X at the top right of the frame end the program when clicked
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         // place the frame in the center of the screen
         frame.setLocationRelativeTo(null);
-
         // add the current game instance's graphical user interface as a component of the frame
         frame.add(gui);
-
         // the frame is invisible by default
         frame.setVisible(true);
     }
@@ -45,7 +39,6 @@ public class Frame implements Info {
      */
     private void setDimensions(Dimension d) {
         frame.setSize(d);
-
         // some aspects of this game were implemented with the default width and height of the
         // canvas in mind. I don't want those values to change
         frame.setMinimumSize(d);
