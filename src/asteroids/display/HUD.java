a0f4f01e6cb332efe40be3b828de79502e6f41ed
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-04-05 01:30:17 -0500

    Add javadocs and comments to the HUD.

--- src/asteroids/display/HUD.java
+++ src/asteroids/display/HUD.java
@@ -4,6 +4,10 @@ import asteroids.Info;
 import asteroids.objects.Player;
 import java.awt.Graphics;
 
+/**
+ * The HUD class is responsible for displaying information to the player, such as the score
+ * and the number of lives remaining.
+ */
 public class HUD implements Info {
     private int numLives;
     private int score;
@@ -15,6 +19,11 @@ public class HUD implements Info {
         playerImage.scale(LIVES_SCALE);
     }
 
+    /**
+     * Render the HUD elements to the screen. The playerImage is a player object that has
+     * been scaled down and is displayed once for every life the player has.
+     * @param g The Canvas's graphics object that we are writing to
+     */
     public void render(Graphics g) {
         for (int i = 0; i < numLives; ++i) {
             playerImage.render(g);
@@ -26,33 +35,54 @@ public class HUD implements Info {
         g.drawString(str, SCORE_X, SCORE_Y);
     }
 
+    /**
+     * This method is called when the Handler initiates a new game. It resets the score,
+     * level, and number of lives for the new game.
+     */
     public void newGame() {
         score = 0;
         numLives = STARTING_LIVES;
         level = 0;
     }
 
-    public int getLevel() {
-        return level;
-    }
-
+    /**
+     * Go to the next level. Called by the Handler when all the asteroids of the current
+     * level have been destroyed.
+     */
     public void nextLevel() {
         ++level;
     }
 
+    /**
+     * Add points to the player's score. This method is called by the handler when the player
+     * shoots another shape (an asteroid or a UFO). This method is also responsible for giving
+     * the player another life if the player's score hits a multiple of 10,000
+     * @param val the points to add to the player's score
+     */
     public void addToScore(int val) {
+        // if the score surpasses a multiple of EXTRA_LIFE_SCORE, add an extra life
         if (score % EXTRA_LIFE_SCORE > (score + val) % EXTRA_LIFE_SCORE) {
             ++numLives;
         }
         score += val;
     }
 
+    /**
+     * Decrease the number of lives by 1. Called when the player collides with another object
+     * (an asteroid, a UFO, a UFO bullet)
+     */
+    public void loseALife() {
+        --numLives;
+    }
+
+    // Getter methods:
+
     public int getScore() {
         return score;
     }
 
-    public void loseALife() {
-        --numLives;
+    public int getLevel() {
+        return level;
     }
 
     public int getNumLives() {
