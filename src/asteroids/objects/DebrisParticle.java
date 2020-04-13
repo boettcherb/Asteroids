7cdf367e2e3c66380bd52470dd1af27c46372568
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-04-13 14:24:25 -0500

    Added comments and javadocs to the DebrisParticle class.

--- src/asteroids/objects/DebrisParticle.java
+++ src/asteroids/objects/DebrisParticle.java
@@ -3,6 +3,12 @@ package asteroids.objects;
 import asteroids.Info;
 import java.util.Random;
 
+/**
+ * Debris Particles are Shapes that consist of an array of points (defined in the Info
+ * class. Debris particles spawn in when other shapes (the player, asteroids, and UFOs)
+ * are destroyed. They only last for about a second, but they create a nice explosion
+ * effect.
+ */
 public class DebrisParticle extends Shape implements Info {
     int life;
 
@@ -14,14 +20,26 @@ public class DebrisParticle extends Shape implements Info {
         this.life = life;
     }
 
+    /**
+     * This method is called by the Handler every frame. Each tick, the debris
+     * particle is moved to a new location (based on its velocity) and its life
+     * is decreased.
+     */
     public void tick() {
         --life;
         translate(getVelX(), getVelY());
     }
 
+    /**
+     * In the constructor, the life of the debris particle is given an initial value.
+     * Every tick, the life of the debris particle decreases until 0. At that point,
+     * it is destroyed.
+     * @return true if the debris particle has no more life, false otherwise
+     */
     public boolean dead() {
         return life <= 0;
     }
 
+    // unused abstract method from the shape class
     public void destruct(boolean play) {}
 }
