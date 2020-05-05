ec22d8adedaf5fc2d31f849c22dbf1d2a5771a50
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-04 22:36:16 -0500

    Added comments and javadocs to the HighScoreList class

--- src/asteroids/util/HighScoreList.java
+++ src/asteroids/util/HighScoreList.java
@@ -5,8 +5,12 @@ import asteroids.Info;
 import java.awt.Graphics;
 import java.util.prefs.Preferences;
 
+/**
+ * The high score list is a part of the Menus that displays the name and high score of the best
+ * players. This information is kept by the JVM across program runs using java preferences.
+ */
 public class HighScoreList implements Info {
-    private Preferences prefs;
+    private final Preferences prefs;
     private String[] names;
     private int newScore;
 
@@ -15,6 +19,10 @@ public class HighScoreList implements Info {
         getNames();
     }
 
+    /**
+     * Create an array of strings and put all the high score names
+     * in the array for easy access.
+     */
     private void getNames() {
         names = new String[NUM_HIGH_SCORES];
         for (int i = 0; i < NUM_HIGH_SCORES; ++i) {
@@ -22,6 +30,11 @@ public class HighScoreList implements Info {
         }
     }
 
+    /**
+     * Render the high score list to the canvas. This method is called by the render method
+     * of the Menu class.
+     * @param g The graphics object of the canvas.
+     */
     public void render(Graphics g) {
         g.setFont(HS_LIST_FONT);
         int height = HS_LIST_Y;
@@ -41,10 +54,27 @@ public class HighScoreList implements Info {
         }
     }
 
+    /**
+     * The High Score List screen has a list of names and scores. The names are on the left and the scores
+     * are on the right. This method determines the number of spaces in between the name and the score and
+     * creates a string of periods of that length. The length of this string will depend on the name and the
+     * score, and will ensure that each line of the list is exactly the same length (name length + spaces +
+     * digits in score = HS_LIST_SPACES).
+     * @param name The name to be added to the high score list
+     * @param score The high score that the player achieved (placed to the right of the name on the list)
+     * @return A string of periods ('.') to separate the name and the score.
+     */
     private String getSpaces(String name, int score) {
         return ".".repeat(HS_LIST_SPACES - name.length() - Integer.toString(score).length());
     }
 
+    /**
+     * This method is called when a game ends. The player's score is passed in and this method checks
+     * if that score is a high score. The new score is a high score if it is greater than the lowest
+     * current high score in the list.
+     * @param score The player's score
+     * @return True if the player's score is a high score, false otherwise.
+     */
     public boolean newPossibleHighScore(int score) {
         if (prefs.getInt(names[NUM_HIGH_SCORES - 1], 0) < score) {
             newScore = score;
@@ -53,6 +83,13 @@ public class HighScoreList implements Info {
         return false;
     }
 
+    /**
+     * This method adds the name of the player that got a new high score to the list. The name
+     * is first formatted by the getValidName() method to prevent duplicates. Then it is inserted
+     * into the list from the back, shifting the lower high scores further down the list (just like
+     * insertion sort)
+     * @param newName The name of the player who will be added to the list
+     */
     public void addNewScore(String newName) {
         String name = getValidName(newName);
         int newIndex = 0;
@@ -70,6 +107,14 @@ public class HighScoreList implements Info {
         newScore = 0;
     }
 
+    /**
+     * A number is added to the end of the name to prevent duplicates. If the name
+     * "Bob" is the new name but it is already in the list, this method will change
+     * this name to "Bob0". This is necessary because recording a score to a duplicate
+     * name will override the original score. This number is not visible on the list.
+     * @param newName The name of the player who will be added to the list
+     * @return The new formatted name with a number at the end
+     */
     private String getValidName(String newName) {
         StringBuilder sb = new StringBuilder(newName);
         if (invalidName(newName)) {
@@ -83,6 +128,12 @@ public class HighScoreList implements Info {
         return sb.toString();
     }
 
+    /**
+     * This method checks if the new name is already in the list. This method is used by the
+     * getValidName() method to see if there is a naming conflict.
+     * @param newName The name of the player who will be added to the list
+     * @return True if the new name is already in the list, false otherwise
+     */
     private boolean invalidName(String newName) {
         for (String name : names) {
             if (name.equals(newName)) {
@@ -91,6 +142,8 @@ public class HighScoreList implements Info {
         }
         return false;
     }
+
+    // Getters and Setters
 
     public int getNewScore() {
         return newScore;
