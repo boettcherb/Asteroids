e79be8e281a2c9da7323375c1f6c7a1000f11642
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-04 22:59:28 -0500

    Added comments and javadocs to the Point class

--- src/asteroids/util/Point.java
+++ src/asteroids/util/Point.java
@@ -1,5 +1,9 @@
 package asteroids.util;
 
+/**
+ * A point is made up of 2 floats, which are coordinates on the screen. Each shape in the
+ * game (the player, the asteroids, etc) is made up of an array of points.
+ */
 public class Point {
     private float X, Y;
 
@@ -8,21 +12,38 @@ public class Point {
         Y = y;
     }
 
+    /**
+     * Move this point x units along the X-axis and y units along the y-axis
+     * @param x the movement along the x-axis
+     * @param y the movement along the y-axis
+     */
     public void translate(float x, float y) {
         X += x;
         Y += y;
     }
 
+    /**
+     * Scale this point's coordinates by a certain factor. The x and y values of this point
+     * are multiplied by the scale factor.
+     * @param scaleValue the scale factor
+     */
     public void scale(float scaleValue) {
         X *= scaleValue;
         Y *= scaleValue;
     }
 
+    /**
+     * Find the distance from this point to another point using the distance formula.
+     * @param other the other point
+     * @return the distance from this point to the other point
+     */
     public float distTo(Point other) {
         float Xdist = X - other.getX();
         float Ydist = Y - other.getY();
         return (float) Math.sqrt(Xdist * Xdist + Ydist * Ydist);
     }
+
+    // Getters and Setters
 
     public float getX() {
         return X;
